e3ee640cbc5800644d5c40f73bbf7af8e203b59e
Juan Carlos Perez <jcarlosp1986@gmail.com>
2020-05-07 16:47:17 -0500

    Refactorizacion modulos

---
+++ src/app/pages/pages.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+
+import { PagesComponent } from './pages.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ProgressComponent } from './progress/progress.component';
+import { Graficas1Component } from './graficas1/graficas1.component';
+import { SharedModule } from '../shared/shared.module';
+import { PAGES_ROUTE } from './pages.route';
+
+
+@NgModule({
+    declarations: [
+        PagesComponent,
+        DashboardComponent,
+        ProgressComponent,
+        Graficas1Component
+    ],
+    exports: [
+        PagesComponent,
+        DashboardComponent,
+        ProgressComponent,
+        Graficas1Component
+    ],
+    imports: [
+        SharedModule,
+        PAGES_ROUTE
+    ]
+})
+export class PagesModule { }
